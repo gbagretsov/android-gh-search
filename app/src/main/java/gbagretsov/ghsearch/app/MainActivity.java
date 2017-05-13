3d492346c06239475ec8317ea03bfa60ad32c500
GBagretsov <gbagretsov@gmail.com>
2017-05-13 18:41:08 +0300

    Подключена более новая версия Gradle, добавлен Action Bar

--- app/src/main/java/gbagretsov/ghsearch/app/MainActivity.java
+++ app/src/main/java/gbagretsov/ghsearch/app/MainActivity.java
@@ -1,15 +1,25 @@
 package gbagretsov.ghsearch.app;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Устанавливаем тулбар
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
