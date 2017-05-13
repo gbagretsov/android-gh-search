5a53da9402097dbd0c5f6d51c372a42668587c94
GBagretsov <gbagretsov@gmail.com>
2017-05-13 20:17:21 +0300

    Добавлены заглушки для вкладок "Поиск" и "Избранное"

--- app/src/main/java/gbagretsov/ghsearch/app/MainActivity.java
+++ app/src/main/java/gbagretsov/ghsearch/app/MainActivity.java
@@ -1,6 +1,12 @@
 package gbagretsov.ghsearch.app;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -15,11 +21,60 @@ public class MainActivity extends AppCompatActivity {
         // Устанавливаем тулбар
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Настраиваем табы
+        ViewPager pager = (ViewPager) findViewById(R.id.viewpager);
+        PagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(pager, true);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    // Этот адаптер оперирует двумя вкладками - "поиск" и "избранное"
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0:
+                    return getResources().getText(R.string.tab_search);
+
+                case 1:
+                    return getResources().getText(R.string.tab_favourite);
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch(position) {
+                case 0:
+                    return new SearchStartFragment();
+
+                case 1:
+                    return new FavouriteFragment();
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
     }
 }
