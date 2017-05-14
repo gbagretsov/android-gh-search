78e39b627059b6ea668296b58886ba3bc9ceb48a
GBagretsov <gbagretsov@gmail.com>
2017-05-14 13:35:19 +0300

    Добавлено сохранение истории поиска

--- app/src/main/java/gbagretsov/ghsearch/app/SearchStartFragment.java
+++ app/src/main/java/gbagretsov/ghsearch/app/SearchStartFragment.java
@@ -3,6 +3,7 @@ package gbagretsov.ghsearch.app;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,8 +11,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
+
+import java.util.*;
 
 
 /**
@@ -26,7 +30,10 @@ public class SearchStartFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    private EditText queryEditText; // Поле для ввода запроса
+    private AutoCompleteTextView queryEditText; // Поле для ввода запроса
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
     /**
      * Use this factory method to create a new instance of
      * this fragment.
@@ -54,9 +61,33 @@ public class SearchStartFragment extends Fragment {
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(fabClickListener);
 
-        queryEditText = (EditText) view.findViewById(R.id.query);
+        queryEditText = (AutoCompleteTextView) view.findViewById(R.id.query);
 
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Получаем настройки, в которых будем хранить и из которых будем брать историю поиска
+        sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+
+        // Получаем историю поиска
+        Map<String, Long> prefsCopy = new HashMap<>(((Map<String, Long>) sharedPref.getAll()));
+        prefsCopy = sortByValueDesc(prefsCopy);
+
+        String[] history = prefsCopy.keySet().toArray(new String[prefsCopy.values().size()]);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (getContext(), android.R.layout.simple_dropdown_item_1line, history);
+        queryEditText.setAdapter(adapter);
+        queryEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queryEditText.showDropDown();
+            }
+        });
     }
 
     @Override
@@ -105,6 +136,12 @@ public class SearchStartFragment extends Fragment {
                 return;
             }
 
+            // Сохраняем запрос в историю вместе с меткой времени (при повторном сохранении того же ключа
+            // сохранится последняя запись с самой новой меткой)
+            editor = sharedPref.edit();
+            editor.putLong(q, Calendar.getInstance().getTimeInMillis());
+            editor.apply();
+
             // Запускаем новую Activity и передаём запрос
             // TODO: при возвращении текущая Activity создаётся заново
             Intent intent = new Intent(context, SearchResultsActivity.class);
@@ -113,4 +150,27 @@ public class SearchStartFragment extends Fragment {
 
         }
     };
+
+    // Сортировка HashMap по убыванию значений. Используется для вывода истории.
+    // Взято здесь: http://stackoverflow.com/questions/109383/sort-a-mapkey-value-by-values-java
+    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValueDesc(Map<K, V> map )
+    {
+        List<Map.Entry<K, V>> list =
+                new LinkedList<>( map.entrySet() );
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+        {
+            @Override
+            public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+            {
+                return ( o2.getValue() ).compareTo( o1.getValue() );
+            }
+        } );
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list)
+        {
+            result.put( entry.getKey(), entry.getValue() );
+        }
+        return result;
+    }
 }
